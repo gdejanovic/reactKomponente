6982e36f3ff0f0a479cb2c1b7ade291cd78ae144
gdejanovic <gorandejanovic@gmail.com>
2021-12-13 10:36:24 +0100

    Added functionality and modules

---
+++ src/components/Info.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Info extends React.Component {
+    render() {
+      return <h1>Učim i šaljem CV okolo :)</h1>;
+    }
+  }
---
+++ src/components/Skills.js
@@ -0,0 +1,3 @@
+export default function Skills (){
+    return <h1>Kažu nauči React bit će ti lakše naći novi posao.</h1>
+}
